8915f2233013560b64f6625f8fa666c86fb6fc2f
Zach Kost-Smith <zachkostsmith@gmail.com>
2012-03-05 21:06:45 -0600

    Export make-gnuplot-setup.

--- package.lisp
+++ package.lisp
@@ -3,6 +3,7 @@
     (:use :cl :toolbox :defclass* :iter)
   (:nicknames :zgp)
   (:export #:gnuplot-setup
+           #:make-gnuplot-setup
            #:data-rep #:func-rep #:save-plot
            #:splot
            #:plot
